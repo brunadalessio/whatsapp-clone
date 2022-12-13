79460ce54636dfb080e510dbbf0b0866b94707a1
bcdalessio <brunodotnet@gmail.com>
2022-12-13 14:20:42 -0300

    Inicialização do projeto de WhatsApp Clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { Avatar } from '@mui/material';
+import React from 'react';
 import './App.css';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='janela__app'>
+      <div className='sidebar'>
+          <header>
+            <Avatar className='header__avatar' src='https://www.w3schools.com/howto/img_avatar.png' alt='Avatar'/>
+            <div className='header__botoes'>
+              <div className='header__icone'>
+                <DonutLargeIcon/>
+              </div>
+              <div className='header__icone'>
+                <ChatIcon/>
+              </div>
+              <div className='header__icone'>
+                <MoreVertIcon/>
+              </div>
+            </div>
+          </header>
+          
+          <div className='buscar'>
+            ...   
+          </div>
+
+          <div className='lista__amigos'>
+            ...
+          </div>
+      </div>
+      <div className='conteudo__area'>
+        ...
+      </div>
     </div>
   );
 }
