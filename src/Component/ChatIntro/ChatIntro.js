640b54a48a4abb583f99792f07326aeb4e7e3b70
bcdalessio <brunodotnet@gmail.com>
2022-12-21 00:16:57 -0300

    Ajustes e imagem da intro

--- src/Component/ChatIntro/ChatIntro.js
+++ src/Component/ChatIntro/ChatIntro.js
@@ -4,8 +4,11 @@ import './ChatIntro.css';
 class ChatIntro extends Component {
     render() { 
         return ( 
-            <div>
-                teste
+            <div className='chatIntro'>
+                <img src='https://media.discordapp.net/attachments/736463094154526810/1054958746327068773/image.png' alt=''/>
+                <h1>WhatsApp Web</h1>
+                <h2>Envie e receba mensagens sem precisar manter seu celular conectado à internet.</h2>
+                <h2>Ao WhatsApp em até quatro aparelhos conectados e um celular ao mesmo tempo.</h2>
             </div>
          );
     }
