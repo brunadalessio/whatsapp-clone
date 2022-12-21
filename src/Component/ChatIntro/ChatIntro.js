0ad5eb4a31aef22adb84a4a4534c308013ef2115
bcdalessio <brunodotnet@gmail.com>
2022-12-21 14:05:54 -0300

    Mais ajustes e finalização da intro das conversas

--- src/Component/ChatIntro/ChatIntro.js
+++ src/Component/ChatIntro/ChatIntro.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './ChatIntro.css';
+import LockIcon from '@mui/icons-material/Lock';
 
 class ChatIntro extends Component {
     render() { 
@@ -7,8 +8,13 @@ class ChatIntro extends Component {
             <div className='chatIntro'>
                 <img src='https://media.discordapp.net/attachments/736463094154526810/1054958746327068773/image.png' alt=''/>
                 <h1>WhatsApp Web</h1>
-                <h2>Envie e receba mensagens sem precisar manter seu celular conectado à internet.</h2>
-                <h2>Ao WhatsApp em até quatro aparelhos conectados e um celular ao mesmo tempo.</h2>
+                <h2>Envie e receba mensagens sem precisar manter seu celular conectado à internet.
+                <br/>
+                Use o WhatsApp em até quatro aparelhos conectados e um celular ao mesmo tempo.</h2>
+
+                <div className='chatIntro__auth'>
+                    <LockIcon className='chatIntro__cadeado'/> Protegido com a criptografia de ponta a ponta
+                </div>
             </div>
          );
     }
